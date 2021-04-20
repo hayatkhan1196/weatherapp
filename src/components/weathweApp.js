de767606c93d2f4403dd1d80ac70c7bb2fc5eb1d
hayatkhan1196 <hayatkhan1196@gmail.com>
2021-04-20 03:33:32 -0700

    i have work on country look up api  and weather app done

--- src/components/weathweApp.js
+++ src/components/weathweApp.js
@@ -5,38 +5,44 @@ function WeathweApp() {
   //   API_Key = '77d96f43e93ba8d3059fd9e0298e76b6';
   const [city, setCity] = useState(null);
   const [search, setSearch] = useState("");
-  const [country,setCountry]=useState(null)
- 
+  const [country, setCountry] = useState(null);
 
-  useEffect(() => {
-    const fetchApi = async () => {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=429736441cf3572838aa10530929f7cd`;
-      const response = await fetch(url);
-      const res = await response.json();
-      console.log(res);
-
-      setCity(res.main);
-      setCountry(res.sys)
-     
+  useEffect(async () => {
     
-    };
-    fetchApi();
-  }, [search]);
+   
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=429736441cf3572838aa10530929f7cd`;
+    const response = await fetch(url);
+    const res = await response.json();
+    console.log(res);
 
+    
+   
+    setCity(res.main);
+
+
+    console.log("city is ",res?.sys?.country);
+    const countryurl = `https://restcountries.eu/rest/v2/alpha/${res?.sys?.country}`;
+    const countryresponse = await fetch(countryurl);
+    const countryres = await countryresponse.json();
+    console.log(countryres);
+    setCountry(countryres.name);
+  }, [search]);
+ 
+  
   return (
     <div className="box">
       <h3 className="weathApp_heading">weather App</h3>
       <div className="inputData">
         <input
           type="search"
-        //   value={search}
+          //   value={search}
           className="inputField"
           placeholder="enter city name"
           onChange={(e) => {
             setSearch(e.target.value);
           }}
         />
-        
+
         {!city || !country ? (
           <p className="page_not_found">not data found</p>
         ) : (
@@ -46,8 +52,8 @@ function WeathweApp() {
                 <i class="fas fa-street-view"></i>
                 {search}
               </h1>
-                    
-              <h1 className="temperature">{country.country}</h1>
+
+              <h1 className="temperature">{country}</h1>
               <h1 className="temperature">{city.temp}°C</h1>
 
               <h3 className="temp_min_max">
